f9edcdcca13c45a1d5ffdeee194e8c0aefd71e6c
Holger Mueller <9jholger@gmail.com>
2018-12-08 15:07:56 -0600

    some styling, began edit profile page

--- public/update.php
+++ public/update.php
@@ -1,3 +1,23 @@
 <?php include "templates/header.php"; ?>
 
+<main class="main">
+    <h1>Edit Profile</h1>
+
+    <form action="" class="edit-profile">
+    <input type="text" class="form-control" placeholder="firstname">
+    <input type="text" class="form-control" placeholder="lastname">
+    <input type="text" class="form-control" placeholder="username">
+    <input type="text" class="form-control" placeholder="location">
+    <input type="text" class="form-control" placeholder="bio">
+    <textarea name="Bio" id="" cols="30" rows="10" placeholder="About me..." class="form-control"></textarea>
+    <input type="text" name="check" value="" style="display:none;">
+    <input type="submit" class="form-control">
+    </form>
+
+    <hr>
+
+    <a href="changePassword.php"><button class="change-password-button form-control">Change Password</button></a>
+
+</main>
+
 <?php include "templates/footer.php"; ?>
