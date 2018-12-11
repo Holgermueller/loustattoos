23c40c891580c80cf730daebc5976c6f09ecdba3
Holger Mueller <9jholger@gmail.com>
2018-12-10 23:03:07 -0600

    began update page

--- public/update.php
+++ public/update.php
@@ -3,20 +3,21 @@
 <main class="main">
     <h1>Edit Profile</h1>
 
-    <form action="" class="edit-profile">
-    <input type="text" class="form-control" placeholder="firstname">
-    <input type="text" class="form-control" placeholder="lastname">
-    <input type="text" class="form-control" placeholder="username">
-    <input type="text" class="form-control" placeholder="location">
-    <input type="text" class="form-control" placeholder="bio">
-    <textarea name="Bio" id="" cols="30" rows="10" placeholder="About me..." class="form-control"></textarea>
+    <form action="includes/update.inc.php" class="edit-profile">
+    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['firstname']?>">
+    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['lastname']?>">
+    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['username']?>">
+    <input type="text" class="form-control" placeholder="<?php echo $_SESSION['location']?>">
+    <textarea name="Bio" id="" cols="30" rows="10" placeholder="<?php echo $_SESSION['bio']?>" class="form-control"></textarea>
     <input type="text" name="check" value="" style="display:none;">
     <input type="submit" class="form-control">
     </form>
 
     <hr>
-
     <a href="changePassword.php"><button class="change-password-button form-control">Change Password</button></a>
+
+    <hr>
+    <a href="profile.php"><button class="cancel-update-button form-control">Cancel</button></a>
 
 </main>
 
