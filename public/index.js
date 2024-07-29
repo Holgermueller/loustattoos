4a77a108fda01a42200415008330592bc83a7e5c
holgermueller <9jholger@gmail.com>
2024-07-29 15:00:09 -0500

    got mobile slideshow working

--- public/index.js
+++ public/index.js
@@ -33,7 +33,34 @@ const showSlides = (n) => {
   dots[slideIndex - 1].className += " active";
 };
 
+const showMobileSlides = (n) => {
+  let i;
+  let slides = document.getElementsByClassName("my-slides-mobile");
+  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+};
+
 showSlides(slideIndex);
+
+showMobileSlides(slideIndex);
 
 const d = new Date();
 let year = d.getFullYear();
